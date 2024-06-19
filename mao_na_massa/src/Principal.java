c2f22f7a8ae33940891987ad81546d3de4c0e6a8
José Felipe Alexandre Martins <felipealexandre178@gmail.com>
2024-06-19 09:10:24 -0300

    feat: code

---
+++ mao_na_massa/src/Principal.java
@@ -0,0 +1,46 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        List<Funcionario> funcionarios = new ArrayList<>();
+
+        // Inserir todos os funcionários
+        funcionarios.add(new Funcionario("Maria", LocalDate.of(2000, 10, 18), new BigDecimal("2009.44"), "Operador"));
+        funcionarios.add(new Funcionario("João", LocalDate.of(1990, 5, 12), new BigDecimal("2284.38"), "Operador"));
+        funcionarios.add(new Funcionario("Caio", LocalDate.of(1961, 5, 2), new BigDecimal("9836.14"), "Coordenador"));
+        funcionarios.add(new Funcionario("Miguel", LocalDate.of(1988, 10, 14), new BigDecimal("19119.88"), "Diretor"));
+        funcionarios.add(new Funcionario("Alice", LocalDate.of(1995, 1, 5), new BigDecimal("2234.68"), "Recepcionista"));
+        funcionarios.add(new Funcionario("Heitor", LocalDate.of(1999, 11, 19), new BigDecimal("1582.72"), "Operador"));
+        funcionarios.add(new Funcionario("Arthur", LocalDate.of(1993, 3, 31), new BigDecimal("4071.84"), "Contador"));
+        funcionarios.add(new Funcionario("Laura", LocalDate.of(1994, 7, 8), new BigDecimal("3017.45"), "Gerente"));
+        funcionarios.add(new Funcionario("Heloísa", LocalDate.of(2003, 5, 24), new BigDecimal("1606.85"), "Eletricista"));
+        funcionarios.add(new Funcionario("Helena", LocalDate.of(1996, 9, 2), new BigDecimal("2799.93"), "Gerente"));
+
+        // Adicionar funcionalidade para buscar funcionário pelo índice
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lista de Funcionários:");
+        for (int i = 0; i < funcionarios.size(); i++) {
+            System.out.println((i + 1) + ". " + funcionarios.get(i).getNome());
+        }
+        System.out.println("Digite o número do funcionário que deseja exibir:");
+        int numeroFuncionario = scanner.nextInt();
+
+        if (numeroFuncionario > 0 && numeroFuncionario <= funcionarios.size()) {
+            Funcionario funcionarioSelecionado = funcionarios.get(numeroFuncionario - 1);
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            System.out.println("Nome: " + funcionarioSelecionado.getNome());
+            System.out.println("Data de Nascimento: " + funcionarioSelecionado.getDataNascimento().format(dateFormatter));
+            System.out.println("Salário: " + String.format("%,.2f", funcionarioSelecionado.getSalario()));
+            System.out.println("Função: " + funcionarioSelecionado.getFuncao());
+        } else {
+            System.out.println("Número inválido.");
+        }
+
+        scanner.close();
+    }
+}
